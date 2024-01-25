d239ea3336ee10d3ff2f1d8bfeaed0017a032bd7
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-01-25 01:04:48 +0000

    To⚡️das las posibles combinaciones de dominios

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let pronoum = ["The", "Our", "Your"];
-  let adj = ["Top", "Fast", "Deep"];
-  let noun = ["Peresiansises", "Capcom", "Waldo", "Cococom", "Ogre"];
-  let extention = ["es", "com", "org"];
+// window.onload = function() {
+let pronoum = ["The", "Our", "Your"];
+let adj = ["Top", "Fast", "Deep"];
+let noun = ["Peresiansises", "Capcom", "Waldo", "Cococom", "Ogre"];
+let extention = ["es", "com", "org"];
 
-  function domainName() {
-    for (let i = 0; i <= 10; i++) {
-      let name = "";
-      let initial = pronoum[Math.floor(Math.random() * pronoum.length)];
-      let mid = adj[Math.floor(Math.random() * adj.length)];
-      let final = noun[Math.floor(Math.random() * noun.length)];
-      let termination = extention[Math.floor(Math.random() * extention.length)];
-
-      if (
-        final.substring(final.length, final.length - termination.length) ===
-        termination
-      ) {
-        final = final.substring(0, final.length - termination.length);
-      }
-
-      name = initial + mid + final + "." + termination;
-
-      console.log(name);
-    }
-  }
-
-  domainName();
-};
+function domainName() {
+  pronoum.forEach((initial) => {
+    adj.forEach((mid) => {
+      noun.forEach((final) => {
+        extention.forEach((termination) => {
+          if (
+            final.substring(final.length, final.length - termination.length) ===
+            termination
+          ) {
+            final = final.substring(0, final.length - termination.length);
+            let name = initial + mid + final + "." + termination;
+            console.log(name);
+          } else {
+            let name = initial + mid + final + "." + termination;
+            console.log(name);
+          }
+        });
+      });
+    });
+  });
+}
+domainName();
+// };
 
 //-------------------------------------------------Solo con FOR LOOPS--------------------------------------------------------------
 
